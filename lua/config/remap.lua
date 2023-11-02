f5a83033d0b8eaec387631a7fc6ac245866f90c3
Slavik Slinyuk <slinyuk.v@hexide-digital.com>
2023-11-02 16:56:48 +0200

    add: navigation between window keybinds

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,8 +1,15 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>o", vim.cmd.NvimTreeFocus)
 vim.keymap.set("n", "<leader>s", vim.cmd.w)
 vim.keymap.set("i", "jj", "<Esc>")
+
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
 -- Telescope
 local builtin = require('telescope.builtin')
 
