90a555ecac12fc92f2ecbefc8b6a3e21d12a1178
Slavik Slinyuk <slinyuk.v@hexide-digital.com>
2023-11-03 21:36:30 +0200

    add: ui foldings

--- lua/config/ui.lua
+++ lua/config/ui.lua
@@ -1,10 +1,15 @@
-vim.o.number = true
-vim.wo.relativenumber = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.expandtab = true
-vim.o.autoindent = true
-vim.o.smartindent = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.foldenable = true
+vim.opt.foldmethod = "indent"
+vim.opt.foldcolumn = "1"
+vim.opt.foldlevel = 99
 
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
