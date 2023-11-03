5d004774c1d9fff64de186283ee75c0721ff0d1a
Slavik Slinyuk <slinyuk.v@hexide-digital.com>
2023-11-03 15:36:23 +0200

    add: Blade emmet support

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,26 +1,16 @@
--- Import the required modules
 local lspconfig = require('lspconfig')
 local mason = require('mason')
 local mason_lspconfig = require('mason-lspconfig')
 
--- Mason setup
-mason.setup({
-    -- Configuration options for mason
-})
+mason.setup({})
 
--- Mason LSP Config setup
 mason_lspconfig.setup({
-    -- Configuration options for mason-lspconfig
     ensure_installed = { 'tsserver', 'emmet_ls', 'intelephense' },
     automatic_installation = true,
 })
 
--- Function to configure LSP on attach
-local on_attach = function(client, bufnr)
-  -- Set up buffer keymaps, etc.
-end
+local on_attach = function(client, bufnr) end
 
--- Common lsp server setup options
 local server_options = {
     on_attach = on_attach,
     flags = {
@@ -28,12 +18,17 @@ local server_options = {
     },
 }
 
--- Loop through the servers specified in `ensure_installed` and set them up
+lspconfig.emmet_ls.setup({
+    filetypes = { 'html', 'css', 'blade', 'javascriptreact', 'typescriptreact', 'blade.php' }, -- Add 'blade.php' here
+    on_attach = on_attach,
+    flags = {
+        debounce_text_changes = 150,
+    },
+})
+
 for _, lsp in ipairs(mason_lspconfig.get_installed_servers()) do
     lspconfig[lsp].setup(server_options)
 end
 
--- Additional server-specific configurations can go here if required
--- Example for PHP using intelephense
 lspconfig.intelephense.setup(server_options)
 
