39d4086f1bc5835c07b70f116f006e5f6d337abe
Slavik Slinyuk <slinyuk.v@hexide-digital.com>
2023-11-30 21:48:58 +0200

    add: init mode

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,32 +1,35 @@
+local run = true
+
 -- Hello message
 print('Road to hell')
-
--- Cunstom files 
-require('config.remap')
 require('config.packer')
-require('config.ui')
+if run then
+  -- Cunstom files 
+  require('config.remap')
+  require('config.ui')
 
--- Nvim Tree
-require("nvim-tree").setup()
+  -- Nvim Tree
+  require("nvim-tree").setup()
 
--- LSP
-require('config.lspconfig')
-require('config.treesitter')
-require('config.cocconfig')
--- require('config.cmpconfig')
+  -- LSP
+  require('config.lspconfig')
+  require('config.treesitter')
+  require('config.cocconfig')
+  -- require('config.cmpconfig')
 
--- BufferLines
-require('bufferline').setup{}
+  -- BufferLines
+  require('bufferline').setup{}
 
--- Nvim comment
-require'nvim_comment'.setup()
+  -- Nvim comment
+  require'nvim_comment'.setup()
 
--- File Type detect
-require('config.ftd')
+  -- File Type detect
+  require('config.ftd')
 
--- Other
-require("which-key").setup{}
-require('config.wk')
-require('config.lualine')
-require('config.miniMove')
-require'colorizer'.setup()
+  -- Other
+  require("which-key").setup{}
+  require('config.wk')
+  require('config.lualine')
+  require('config.miniMove')
+  require'colorizer'.setup()
+end
